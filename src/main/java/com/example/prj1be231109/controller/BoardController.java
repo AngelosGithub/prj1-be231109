4e1a9f2a3e8174a218f17c25291bcdb1e573b8b6
Angelo <ljy6037@gmail.com>
2023-11-09 11:51:46 +0900

    최초설정 및 BoardController post /api/board/add 작성

--- src/main/java/com/example/prj1be231109/controller/BoardController.java
+++ src/main/java/com/example/prj1be231109/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
 public class BoardController {
+
     @PostMapping("add")
     public void add() {
         System.out.println("BoardController.add");
