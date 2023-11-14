19a0b93b7fb5ef36a2e530709ecff00a3277c27f
Angelo <ljy6037@gmail.com>
2023-11-14 09:39:35 +0900

    231113 로그인 기능 구현중

--- src/main/java/com/example/prj1be231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be231109/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.example.prj1be231109.controller;
 import com.example.prj1be231109.domain.Member;
 import com.example.prj1be231109.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -86,6 +88,16 @@ public class MemberController {
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping("login")
+    public ResponseEntity login(@RequestBody Member member) {
+        System.out.println("member = " + member);
+        if (service.login(member)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 }
