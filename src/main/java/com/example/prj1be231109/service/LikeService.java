fe8ed58fcea821a21740281ea328a648c5849f15
Angelo <ljy6037@gmail.com>
2023-11-16 16:29:28 +0900

    231116 좋아요 기능 클릭시 db에 추가되었다가 지워지는것 확인

--- src/main/java/com/example/prj1be231109/service/LikeService.java
+++ src/main/java/com/example/prj1be231109/service/LikeService.java
@@ -1,6 +1,7 @@
 package com.example.prj1be231109.service;
 
 import com.example.prj1be231109.domain.Like;
+import com.example.prj1be231109.domain.Member;
 import com.example.prj1be231109.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class LikeService {
+
     private final LikeMapper mapper;
 
-    public void update(Like like) {
+    public void update(Like like, Member login) {
+
+        like.setMemberId(login.getId());
+
         // 처음 좋아요를 누를때 > insert
         // 다시 누르면 > delete
         int count = 0;
